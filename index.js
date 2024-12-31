a9d6ef2d3c435cd074c9ac106e20424d994d3641
Answayyys <Answays@outlook.com>
2024-12-31 12:39:23 +0400

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Fait bour Joke", "Fait bour Laktoo", "Fait bour Heka", "Fait bour Tidjay"];
+const statusMessages = ["Fait bour Joke :middle_finger:", "Fait bour Laktoo :smiling_imp:", "Fait bour Heka :clown:", "Fait bour Tidjay :cold_face:"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
