aaea0d53afd0676b01a45fa172b15f43b85d6884
Answays <Answays@outlook.com>
2024-12-31 14:23:51 +0400

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Bon reveillon à tous 🎉", "Bon reveillon à tous ✨", "Bon reveillon à tous 🥳", "Bon reveillon à tous 🎊", "Bon reveillon à tous 🔥"];
+const statusMessages = ["Bon reveillon à tous 🎉", "Bon reveillon à tous ✨", "Bon reveillon à tous 🥳", "Bonne année 🎊", "Bon reveillon à tous 🔥"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
